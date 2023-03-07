9b9d3293e5a24193c6f6b79ac79941a8fef50f5f
UgurTheG <49240783+UgurTheG@users.noreply.github.com>
2023-03-07 21:29:10 +0100

    further refactoring

--- js/events/events.js
+++ js/events/events.js
@@ -1,7 +1,7 @@
 class eventEntity {
-    constructor(eventInstance, eventObjects) {
-        this.eventInstance = eventInstance;
-        this.eventObjects = eventObjects;
+    constructor(instance, objects) {
+        this.instance = instance;
+        this.objects = objects;
     }
 }
 
@@ -9,22 +9,20 @@ let events = document.querySelector(".eintrag");
 let event_parent_current = document.querySelector("#current");
 let event_parent_next = document.querySelector("#next");
 
-let month = new Date().getMonth() + 1
-let n_month = month % 12 + 1
+let currentMonth = new Date().getMonth() + 1
+let currentYear = new Date().getFullYear()
 
-let year = new Date().getFullYear()
+let nextMonth = currentMonth % 12 + 1
 
-const months = ['Januar','Februar','März','April','Mai','Juni','Juli','August','September','Oktober','November','Dezember'];
-
-document.querySelector("#kalendermonat").innerHTML = months[month-1]
+document.querySelector("#kalendermonat").innerHTML = new Intl.DateTimeFormat('de', { month: 'long' }).format(new Date())
 
 fetch("../resources/data/events/events.json")
 .then((response) => {
     return response.json();
 })
 .then((data) => {
-    [new eventEntity(event_parent_current, data[`${month}_${year}`]), new eventEntity(event_parent_next, data[`${n_month}_${year}`])].forEach(eventObject => {
-        insertEvents(eventObject.eventInstance, eventObject.eventObjects)
+    [new eventEntity(event_parent_current, data[`${currentMonth}_${currentYear}`]), new eventEntity(event_parent_next, data[`${nextMonth}_${currentYear}`])].forEach(eventEntry => {
+        insertEvents(eventEntry.instance, eventEntry.objects)
     });
     
     event_parent_current.removeChild(events);
@@ -34,17 +32,16 @@ function insertEvents(eventParentObject, eventOccurenceList) {
     eventOccurenceList.forEach(eventOccurence => {
         const event_clone = events.cloneNode(true);
         eventParentObject.appendChild(event_clone);
-        
-        const { event, header, text, time, place, image } = eventOccurence;
-        insertData(event_clone, event, header, text, time, place, image);
+    
+        insertData(event_clone, eventOccurence);
     })
 }
 
-function insertData(event_clone, header, event, text, time, place, image) {
-    event_clone.querySelector(".name").innerHTML = event;
-    event_clone.querySelector(".event").innerHTML = header;
-    event_clone.querySelector(".text").innerHTML = text;
-    event_clone.querySelector(".time p").innerHTML = time;
-    event_clone.querySelector(".place p").innerHTML = place
-    event_clone.querySelector(".imageArea img").src = image
+function insertData(event_clone, eventOccurence) {
+    event_clone.querySelector(".name").innerHTML = eventOccurence.event;
+    event_clone.querySelector(".event").innerHTML = eventOccurence.header;
+    event_clone.querySelector(".text").innerHTML = eventOccurence.text;
+    event_clone.querySelector(".time p").innerHTML = eventOccurence.time;
+    event_clone.querySelector(".place p").innerHTML = eventOccurence.place;
+    event_clone.querySelector(".imageArea img").src = eventOccurence.image;
 }
